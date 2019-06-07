3b367d24f1463acf09cbef94e6a4f1e220216efa
Luke Rasmussen <luke.rasmussen@gmail.com>
2019-06-07 12:29:28 -0500

    Initial add of code

---
+++ RDotNetExample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using RDotNet;
+
+namespace RDotNetExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Engine = REngine.GetInstance();
+            var result = Engine.Evaluate(
+              @"setNames(data.frame(
+                matrix(c(1,2,3,4,5,6),nrow=3,ncol=2)),
+                c(""a"",""b""))");
+            if (result.IsDataFrame()) {
+              var data = result.AsDataFrame();
+              int numCol = data.ColumnCount;
+              for (int colIdx = 0; colIdx < numCol; colIdx++) {
+                var column = data[colIdx].AsCharacter();
+                int numRow = column.Length;
+                for (int rowIdx = 0; rowIdx < numRow; rowIdx++) {
+                  Console.WriteLine(column[rowIdx]);
+                }
+              }
+            }
+
+        }
+    }
+}
